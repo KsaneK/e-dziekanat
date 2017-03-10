36dfae4095a9fdfb8a02d412a731ef72442f21fe
Łukasz Olender <ukasz Olender>
2017-03-10 20:19:19 +0100

    Dodane funkcje get_timetable() oraz parse()

--- plan.py
+++ plan.py
@@ -44,6 +44,58 @@ class eDziekanat:
             data = self.params,
             verify = False
         )
+    def get_timetable(self, group):
+        """Pobranie planu zajęć grupy"""
+        t = URL + "logged.php?sid=%s&mid=328&iid=20162&vrf=32820162&rdo=1&pos=0&exv=%s&opr=DTXT" % (self.ssid, group)
+        timetable_req = self.session.get(t, verify=False)
+        # próby pobrania zawartości pliku
+        # e-dziekanat był robiony dawno temu i nie wszystko działa jak powinno :D
+        for i in range(10):
+            print("Pobieranie planu grupy %s, próba %d" % (group, i+1), end='')
+            sleep(5)
+            try:
+                timetable_req = self.session.get(t, verify=False)
+            except ConnectionError as e:
+                print(e)
+                exit()
+            if len(timetable_req.content):
+                print("\t\t[OK]")
+                break
+            else:
+                print("\t\t[FAIL]")
+        self.parse(timetable_req.content.decode("iso-8859-2").split("\r\n"))
+    def parse(self, lessons):
+        # zamiany godziny rozpoczęcia zajęć na numer bloku
+        print("Przetwarzanie pliku... ", end='')
+        hours = {
+            "08:00": "0",
+			"09:50": "1",
+			"11:40": "2",
+			"13:30": "3",
+			"15:45": "4",
+			"17:35": "5",
+			"19:25": "6"
+        }
+        for lesson in lessons[1:]:
+            if not lesson:
+                continue
+            l = lesson.split(",")
+            l[3] = hours[l[3]]
+            # index końca nazwy przedmiotu
+            index_of_lesson_type = l[0].index(" (")
+            # zapisanie typu zajęć(w/ć/L) do zmiennej
+            l_type = l[0][index_of_lesson_type+2:index_of_lesson_type+3]
+
+            l[0] = l[0][:index_of_lesson_type]
+            # dodanie typu zajęć do listy
+            l.insert(1, l_type)
+            if not l[3] in self.timetable:
+                #jeżeli slownik nie posiada klucza dla danego dnia to go tworzymy
+                self.timetable[l[3]] = [[] for i in range(7)]
+            # wpisanie bloku zajęć w odpowiednie miejsce na liście
+            self.timetable[l[3]][int(l[4])] = [l[0], l[1], l[2]]
+        print("\t\t\t\t[OK]")
+
 
 if __name__ == "__main__":
     with open("dane.txt", "r") as f:
@@ -54,3 +106,4 @@ if __name__ == "__main__":
         groups = groups.split(",")
     ed = eDziekanat(username, password, output)
     ed.login()
+    ed.get_timetable("I6Y4S1")
